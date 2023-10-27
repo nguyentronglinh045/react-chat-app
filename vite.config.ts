33ea22b6ccfb704f2ce421a9841ec1a2f36238c7
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-10-27 16:22:13 +0700

    install dotenv, firebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 import path from 'path'
+import { defineConfig } from 'vite'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
